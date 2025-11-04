0601c5ba51d02f83f04ef54ab3e31ef9d0bc333e
Crisjonh26 <114681490+Crisjonh26@users.noreply.github.com>
2025-11-05 07:47:53 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+curl --request GET 
+	--url https://inboxes-com.p.rapidapi.com/domains 
+	--header 'x-rapidapi-host: inboxes-com.p.rapidapi.com' 
+	--header 'x-rapidapi-key: bfc8e325bbmsh43c767b33029b50p1f8e92jsn695a8ea52a1b'
