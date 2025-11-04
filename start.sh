cdca1fea18c9b79f7f9c360468f9741bfe772d8e
Crisjonh26 <114681490+Crisjonh26@users.noreply.github.com>
2025-11-05 07:49:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,24 @@
-curl --request GET 
-	--url https://inboxes-com.p.rapidapi.com/domains 
-	--header 'x-rapidapi-host: inboxes-com.p.rapidapi.com' 
-	--header 'x-rapidapi-key: bfc8e325bbmsh43c767b33029b50p1f8e92jsn695a8ea52a1b'
+import Foundation
+
+let headers = [
+	"x-rapidapi-key": "bfc8e325bbmsh43c767b33029b50p1f8e92jsn695a8ea52a1b",
+	"x-rapidapi-host": "inboxes-com.p.rapidapi.com"
+]
+
+let request = NSMutableURLRequest(url: NSURL(string: "https://inboxes-com.p.rapidapi.com/domains")! as URL,
+                                        cachePolicy: .useProtocolCachePolicy,
+                                    timeoutInterval: 10.0)
+request.httpMethod = "GET"
+request.allHTTPHeaderFields = headers
+
+let session = URLSession.shared
+let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+	if (error != nil) {
+		print(error as Any)
+	} else {
+		let httpResponse = response as? HTTPURLResponse
+		print(httpResponse)
+	}
+})
+
+dataTask.resume()
